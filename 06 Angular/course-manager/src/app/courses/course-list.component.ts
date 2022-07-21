e4767a36786a230da9aaa1a55731920211d40498
fabriciosilva03 <fabriciosilva03@hotmail.com>
2022-07-20 23:48:46 -0300

    angular

---
+++ 06 Angular/course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html',
+   
+})
+
+export class CourseListComponent implements OnInit{
+
+    courses: Course[] =  [];
+
+    ngOnInit(): void{
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular',
+                imageUrl: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'December, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '/assets/images/http.png',
+                price: 45.90,
+                code: 'LKL-8796',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November, 5, 2019'
+            }
+
+            
+        ]
+    }
+
+}
